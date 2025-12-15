1557f0486405f7f413a5578ae5b7ac9e20baff06
clelele <2513502304@qq.com>
2025-12-16 00:47:26 +0800

    chore: change logger config

--- src/waifuboard/utils.py
+++ src/waifuboard/utils.py
@@ -3,10 +3,16 @@ import logging
 
 # 日志记录
 logging.basicConfig(
-    format='%(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
+    format="%(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
     level=logging.INFO,
-    handlers=[RichHandler()],
+    handlers=[
+        RichHandler(
+            rich_tracebacks=True,
+            tracebacks_show_locals=True,
+            tracebacks_suppress=[],
+            tracebacks_max_frames=100,
+        )
+    ],
 )
-
-logger = logging.getLogger('WaifuBoard')
+logger = logging.getLogger("WaifuBoard")
