059ff16173726a66ed8f7744ac8d96b4d3b2b717
clelele <2513502304@qq.com>
2025-12-16 18:20:46 +0800

    chore: export Booru class as base client class

--- src/waifuboard/__init__.py
+++ src/waifuboard/__init__.py
@@ -1,5 +1,6 @@
 """WaifuBoard: Asynchronous API for downloading images, tags, and metadata from image board sites (e.g., Danbooru, Safebooru, Yandere). Ignore the downloaded files."""
 
+from .booru import Booru
 from .danbooru import DanbooruClient
 from .safebooru import SafebooruClient
 from .moebooru import YandereClient
@@ -8,10 +9,11 @@ from .moebooru import YandereClient
 __author__ = "ChijiangZhai"
 __email__ = "chijiangzhai@gmail.com"
 __description__ = """Asynchronous API for downloading images, tags, and metadata from image board sites (e.g., Danbooru, Safebooru, Yandere). Ignore the downloaded files."""
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __all__ = [
-    'DanbooruClient',
-    'SafebooruClient',
-    'YandereClient',
+    "Booru",
+    "DanbooruClient",
+    "SafebooruClient",
+    "YandereClient",
 ]
